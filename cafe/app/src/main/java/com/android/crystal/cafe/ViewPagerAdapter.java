d1935201c547d88d8a5506186f25a50b70aa0027
Crystal Yang <ydh1031@gmail.com>
2016-07-23 23:09:21 -0700

    first commit

---
+++ cafe/app/src/main/java/com/android/crystal/cafe/ViewPagerAdapter.java
@@ -0,0 +1,52 @@
+package com.android.crystal.cafe;
+
+/**
+ * Created by Crystal on 7/23/16.
+ */
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    final int PAGE_COUNT = 2;
+    // Tab Titles
+    private String tabtitles[] = new String[] { "Tab1", "Tab2"};
+    Context context;
+
+    public ViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+   //@Override
+    public int getCount() {
+        return PAGE_COUNT;
+    }
+
+    //@Override
+    public Fragment getItem(int position) {
+        switch (position) {
+
+            // Open FragmentTab1.java
+            case 0:
+                MapFragment fragmenttab1 = new MapFragment();
+                return fragmenttab1;
+
+            // Open FragmentTab2.java
+            case 1:
+                ListFragment fragmenttab2 = new ListFragment();
+                return fragmenttab2;
+
+        }
+        return null;
+    }
+
+   // @Override
+    public CharSequence getPageTitle(int position) {
+        return tabtitles[position];
+    }
+
+
+}
